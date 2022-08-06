c6e18ec534a579b705a39f616401de1986eed14c
HidanZZ <aitdaoudelhoussein@gmail.com>
2022-08-06 16:47:42 +0100

    added loading animation

--- src/components/layouts/main.js
+++ src/components/layouts/main.js
@@ -1,20 +1,41 @@
 import Head from "next/head";
 import { Box, Container } from "@chakra-ui/react";
 import Navbar from "../Navbar";
+import { motion } from "framer-motion";
 import WithSubnavigation from "../nav";
 import Footer from "../Footer";
-const Main = ({ children, router }) => {
+import ThreeDotsWave from "../ThreeDotsWave";
+const Main = (props) => {
+  const variants = {
+    visible: { opacity: 1 },
+    invisible: {
+      opacity: 0,
+    },
+  };
   return (
-    <Box as="main" overflow={"hidden"} position="relative">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Portfilio</title>
-      </Head>
-      <WithSubnavigation></WithSubnavigation>
-      <Box w="full" maxW="100vw">
-        {children}
-      </Box>
-      {/* <Footer></Footer> */}
+    <Box>
+      {!props.loaded && <ThreeDotsWave />}
+      <motion.div
+        as="main"
+        variants={variants}
+        initial="invisible"
+        animate={props.loaded ? "visible" : "invisible"}
+        style={{
+          overflow: "hidden",
+          position: "relative",
+          // opacity: props.loaded ? "1" : "0",
+        }}
+      >
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Portfilio</title>
+        </Head>
+        <WithSubnavigation></WithSubnavigation>
+        <Box w="full" maxW="100vw">
+          {props.children}
+        </Box>
+        {/* <Footer></Footer> */}
+      </motion.div>
     </Box>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,34 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Main from "../components/layouts/main";
 import theme from "../utils/theme";
-import { RemoveScrollBar } from "react-remove-scroll-bar";
+import { useRive, Layout, Fit, Alignment } from "@rive-app/react-canvas";
+import { useState } from "react";
 const App = ({ Component, pageProps, router }) => {
+  const [loaded, setLoaded] = useState(false);
+  const { rive, RiveComponent } = useRive({
+    src: "hero-animation.riv",
+    autoplay: true,
+    stateMachines: "scene",
+    layout: new Layout({
+      fit: Fit.Cover,
+      alignment: Alignment.Center,
+    }),
+    onLoad: () => {
+      console.log("loaded");
+      setTimeout(() => setLoaded(true), 1000);
+      // setLoaded(true);
+    },
+  });
   return (
     <ChakraProvider theme={theme}>
       {/* <RemoveScrollBar /> */}
-      <Main router={router}>
-        <Component {...pageProps} key={router.route}></Component>
+      <Main
+        //pass the loaded state to the main component
+        loaded={loaded}
+      >
+        <Component {...pageProps} key={router.route}>
+          <RiveComponent style={{ minHeight: "100vh" }} />
+        </Component>
       </Main>
     </ChakraProvider>
   );
