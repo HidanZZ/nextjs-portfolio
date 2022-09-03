f302d447825d94b9b1ec3970356bc567b15fefc9
HidanZZ <aitdaoudelhoussein@gmail.com>
2022-09-03 17:52:32 +0100

    added google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Main from "../components/layouts/main";
 import theme from "../utils/theme";
 import "../css/global.css";
+import { useEffect } from "react";
+import Router from "next/router";
+import { initGA, logPageView } from "../analytics";
 const App = ({ Component, pageProps, router }) => {
+  useEffect(() => {
+    initGA();
+    logPageView();
+    Router.events.on("routeChangeComplete", logPageView);
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <Main
