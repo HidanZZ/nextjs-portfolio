84bd926ada196379df18fb992e0ef1391d9a1fc4
HidanZZ <aitdaoudelhoussein@gmail.com>
2022-09-05 12:10:22 +0100

    fixed animation position

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -364,15 +364,17 @@ export default function ContactFormWithSocialButtons({ innerRef, mt }) {
                     <Box
                       as={motion.div}
                       position="absolute"
-                      top={0}
-                      left="0"
-                      right="0"
-                      bottom="0"
+                      top="50%"
+                      left="50%"
+                      transform="translate(-50%, -50%)"
+                      w={"70%"}
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       exit={{ opacity: 0 }}
                     >
                       <Lottie
+                        width={"100%"}
+                        height={"100%"}
                         animationData={mailSent}
                         loop={false}
                         onComplete={() => setIsSent(false)}
